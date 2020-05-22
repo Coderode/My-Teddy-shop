8e439b6f85cf8fd2883b1c6febc6ea8876da7853
Sandeep <sk9958814616@gmail.com>
2020-05-22 11:14:07 +0530

    First commit for the website

---
+++ shop/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Product
+from math import ceil
+
+# Create your views here.
+def index(request):
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+        
+    params = {'allProds':allProds}
+    return render(request,'shop/index.html',params)
+
+def about(request):
+    return render(request,'shop/about.html')
+
+def contact(request):
+    return HttpResponse("we are at contact page")
+
+def tracker(request):
+    return HttpResponse("we are at tracker page")
+
+def search(request):
+    return HttpResponse("we are at search page")
+
+def productView(request):
+    return HttpResponse("we are at product view page")
+
+def checkout(request):
+    return HttpResponse("we are at checkout page")
+
