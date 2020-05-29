0d29f700084d4426b58fb96ab35892d61ab5433e
Sandeep <sk9958814616@gmail.com>
2020-05-29 18:11:32 +0530

    started blog index , blogpost

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.contrib import messages
+from . models import Blogpost
+
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    allPosts = Blogpost.objects.all()
+    params = {'allPosts':allPosts}
+    return render(request, 'blog/index.html',params)
+
+def blogpost(request, id):
+    total = Blogpost.objects.all().count()
+    post = Blogpost.objects.filter(post_id = id)[0]  #it returns array and it has only one object so taking 0 index
+    if id <= 1 :
+        nextPost = Blogpost.objects.filter(post_id = id+1)[0]
+        prevPost = {'obj': 'null'}
+    elif id >= total :
+        nextPost = {'obj': 'null'}
+        prevPost = Blogpost.objects.filter(post_id = id-1)[0]
+    else :
+        nextPost = Blogpost.objects.filter(post_id = id+1)[0]
+        prevPost = Blogpost.objects.filter(post_id = id-1)[0]
+    param = {'post':post,'nextPost':nextPost,'prevPost':prevPost}
+    return render(request, 'blog/blogpost.html',param)
